841198cdf90e0d98ddd3cad444d94d239c8739ff
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-01-10 09:47:59 +0530

    refactoring p-2 using Observable

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -8,23 +8,26 @@ import { CounterState } from '../state/counter.state';
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
+export class CounterOutputComponent implements OnInit {
   // @Input() counter: any;
   counter: number = 0;
+  counter$!: Observable<CounterState>;
   counterSubscription!: Subscription;
   constructor(private store: Store<{ counter: CounterState }>) {}
 
   ngOnInit(): void {
-    this.counterSubscription = this.store
-      .select('counter')
-      .subscribe((data) => {
-        this.counter = data.counter;
-      });
+    // this.counterSubscription = this.store
+    //   .select('counter')
+    //   .subscribe((data) => {
+    //     this.counter = data.counter;
+    //   });
+
+    this.counter$ = this.store.select('counter');
   }
 
-  ngOnDestroy(): void {
-    if (this.counterSubscription) {
-      this.counterSubscription.unsubscribe();
-    }
-  }
+  // ngOnDestroy(): void {
+  //   if (this.counterSubscription) {
+  //     this.counterSubscription.unsubscribe();
+  //   }
+  // }
 }

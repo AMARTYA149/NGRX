84a1d9b6e94404d53bf7b8648218e197afb5e68e
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-02-12 19:35:24 +0530

    implement routing & installed ngrx-store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CounterButtonComponent } from './counter/counter-button/counter-button.component';
@@ -9,6 +10,9 @@ import { CounterOutputComponent } from './counter/counter-output/counter-output.
 import { CounterComponent } from './counter/counter/counter.component';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { counterReducer } from './counter/state/counter.reducer';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +21,14 @@ import { counterReducer } from './counter/state/counter.reducer';
     CounterButtonComponent,
     CounterOutputComponent,
     CustomCounterInputComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostsListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
   ],
   providers: [],

68355f4ccaace2526e2df7829952f515b76ee276
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-02-23 17:44:20 +0530

    get async data from ngrx store

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
+import { AppState } from 'src/app/store/app.state';
 import { getCounter } from '../state/counter.selectors';
 
 import { CounterState } from '../state/counter.state';
@@ -15,7 +16,7 @@ export class CounterOutputComponent implements OnInit {
   counter: number = 0;
   counter$!: Observable<number>;
   counterSubscription!: Subscription;
-  constructor(private store: Store<{ counter: CounterState }>) {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     // this.counterSubscription = this.store

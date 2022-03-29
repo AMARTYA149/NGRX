5ba563068282323e5503a5858d1f0efb476dd9e5
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-03-29 22:08:13 +0530

    save user token data in ngrx state

--- src/app/auth/state/auth.reducer.ts
+++ src/app/auth/state/auth.reducer.ts
@@ -1,7 +1,17 @@
-import { Action, createReducer } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
+import { loginSuccess } from './auth.actions';
 import { AuthState, initialState } from './auth.state';
 
-const _authReducer = createReducer(initialState);
+const _authReducer = createReducer(
+  initialState,
+  on(loginSuccess, (state, action) => {
+    // console.log(action);
+    return {
+      ...state,
+      user: action.user,
+    };
+  })
+);
 
 export function AuthReducer(state: AuthState | undefined, action: Action) {
   return _authReducer(state, action);

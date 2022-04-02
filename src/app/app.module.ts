65a2cbb14a92602bb1bcc6d4f637d3a22802d728
Amartya Aishwarya <iam_amartyaaishwarya@Amartyas-MacBook-Air.local>
2022-04-02 19:51:05 +0530

    shared loading spinned implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,16 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { appReducer } from './store/app.state';
 import { EffectsModule } from '@ngrx/effects';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 // import { postsReducer } from './posts/state/posts.reducer';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoadingSpinnerComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -24,7 +30,7 @@ import { EffectsModule } from '@ngrx/effects';
     ReactiveFormsModule,
     HttpClientModule,
     EffectsModule.forRoot([]),
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({
       logOnly: environment.production, // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,14 +1,19 @@
-import { counterReducer } from '../counter/state/counter.reducer';
-import { CounterState } from '../counter/state/counter.state';
-import { postsReducer } from '../posts/state/posts.reducer';
-import { PostsState } from '../posts/state/posts.state';
+// import { counterReducer } from '../counter/state/counter.reducer';
+// import { CounterState } from '../counter/state/counter.state';
+// import { postsReducer } from '../posts/state/posts.reducer';
+// import { PostsState } from '../posts/state/posts.state';
+import { SharedReducer } from './Shared/shared.reducer';
+import { SHARED_STATE_NAME } from './Shared/shared.selector';
+import { SharedState } from './Shared/shared.state';
 
 export interface AppState {
-  counter: CounterState;
-  posts: PostsState;
+  // counter: CounterState;
+  // posts: PostsState;
+  [SHARED_STATE_NAME]: SharedState;
 }
 
 export const appReducer = {
-  counter: counterReducer,
-  posts: postsReducer,
+  // counter: counterReducer,
+  // posts: postsReducer,
+  [SHARED_STATE_NAME]: SharedReducer,
 };

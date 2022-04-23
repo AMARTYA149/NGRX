ca6077675ad46f24214e4613e6ebf42f47f7d57f
AMARTYA149 <amartya970914@gmail.com>
2022-04-23 15:17:05 +0530

    IMP - logout bug fix

--- src/app/services/AuthToken.interceptor.ts
+++ src/app/services/AuthToken.interceptor.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 import { AppState } from '../store/app.state';
 import { Store } from '@ngrx/store';
 import { getToken } from '../auth/state/auth.selector';
-import { exhaustMap } from 'rxjs/operators';
+import { exhaustMap, take } from 'rxjs/operators';
 
 @Injectable()
 export class AuthTokenInterceptor implements HttpInterceptor {
@@ -19,6 +19,7 @@ export class AuthTokenInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return this.store.select(getToken).pipe(
+      take(1),
       exhaustMap((token) => {
         if (!token) {
           return next.handle(req);
